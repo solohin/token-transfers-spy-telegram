dbdd6e921bed84c94273e40349714dff14b91cab
Ilya Solokhin <solohin.i@gmail.com>
2017-12-01 21:37:31 +0700

    Wrong message

---
+++ functions/handlers/onUnkownMessage.test.js
@@ -0,0 +1,26 @@
+"use strict";
+jest.mock('../components/sendTelegramMessage.js');
+jest.mock('../components/isAdmin.js');
+jest.mock('../components/db.js');
+//libs
+const assert = require('chai').assert;
+//modules
+const onMessage = require('../components/onMessage');
+const db = require('../components/db');
+const sendTelegramMessage = require('../components/sendTelegramMessage.js');
+//init
+
+const CHAT_ID = 12333;
+
+describe('/start message', () => {
+    beforeEach(() => {
+        sendTelegramMessage.mockClear();
+    });
+
+    it('should send any message', done => {
+        onMessage(CHAT_ID, 'abracadabra').then(()=>{
+            assert.strictEqual(1, sendTelegramMessage.mock.calls.length);
+            done();
+        })
+    });
+});
